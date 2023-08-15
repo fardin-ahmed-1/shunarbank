b635c8adbeb5ffdb3ceddb7de6bcc485ef941dcd
fardin-ahmed-1 <contactfardin22@gmail.com>
2023-08-15 11:05:41 +0600

    make

--- deposit.js
+++ deposit.js
@@ -1,9 +1,21 @@
 document.getElementById('deposit_request').addEventListener('click',function(){
     const depositInput=document.getElementById('Deposit_input');
-    const depositValueString=depositInput.value;
-    const depositValue=parseFloat(depositValueString)
+    const newDepositAmountstring=depositInput.value;
+    const newDepositAmount=parseFloat(newDepositAmountstring)
     
-    console.log(typeof depositValue)
+    
+    const depositTotalElement=document.getElementById('deposit_total');
+    const privousDepositTotalstring=depositTotalElement.innerText;
+    const privousDepositTotal=parseFloat(privousDepositTotalstring)
+    const currenctDepositTotal=privousDepositTotal+newDepositAmount
+    depositTotalElement.innerText=currenctDepositTotal
 
+
+     const balanceTotalElement=document.getElementById('balance');
+     const balanceTotalstrin=balanceTotalElement.innerText;
+     const balanaceTotal=parseFloat(balanceTotalstrin)
+      balanceTotalElement.innerText=balanaceTotal+newDepositAmount;
+
+      console.log(newDepositAmount)
     depositInput.value=''
 })
